56384bb60ed70579873dcab9330b9e622af5056c
Aditya202006 <adityakanchana@gmail.com>
2024-09-13 10:31:44 +0530

    Reverse_a_string.py

---
+++ Reverse_a_string.py
@@ -0,0 +1,9 @@
+# Write a python program to reverse a given string
+
+input_string=input('Enter a String :')
+print('Reversed string is',input_string[::-1])
+
+'''
+output:
+Enter a String :python
+Reversed string is nohtyp
