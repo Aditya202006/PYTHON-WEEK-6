c354d738563638d37ea8c2f79220b13c1984fb45
Aditya202006 <adityakanchana@gmail.com>
2024-09-13 11:23:38 +0530

    Add_key-value_to_dictionary.py

---
+++ Add_key-value_to_dictionary.py
@@ -0,0 +1,9 @@
+# write a program to add a new key-value pair to an eixting dictionary
+
+s={1:'p',2:'y',3:'t',4:'h',5:'o',6:'n'}
+s[7]='m'
+print(s)
+
+'''
+output:
+{1: 'p', 2: 'y', 3: 't', 4: 'h', 5: 'o', 6: 'n', 7: 'm'}
