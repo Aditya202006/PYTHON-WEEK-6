330b5c51751eadd53b5a7ebf2900b305e81fc6a0
Aditya202006 <adityakanchana@gmail.com>
2024-09-13 10:36:07 +0530

    Reverse_string_forloop.py

---
+++ reverse_string_forloop.py
@@ -0,0 +1,12 @@
+# Write a python program to reverse a given string without using a slicing operator 
+
+input_string=input('Enter a String :')
+reverse=""
+for i in input_string=:
+	reverse = i + reverse
+print('Reversed string is ',reverse)
+
+'''
+output:
+Enter a String :java
+Reversed string is avaj
