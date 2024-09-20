f7a539e42ce51eb8446adcd5d5f503352d96f50e
Aditya202006 <adityakanchana@gmail.com>
2024-09-20 12:16:54 +0530

    Sum_of_items_in_list.py

---
+++ Sum_of_items_in_list.py
@@ -0,0 +1,13 @@
+#  Write a progra, to sum all the items in a given sictionary
+
+s = {1: 'p', 2: 'y', 3: 't', 4: 'h', 5: 'o', 6: 'n'}
+dic = s.values()
+sum1 = ''
+for i in dic:
+    sum1 += i
+print(sum1)
+
+'''
+Output:
+python
+'''
